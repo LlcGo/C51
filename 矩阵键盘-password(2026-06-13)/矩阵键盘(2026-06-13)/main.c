0c3d1f57dc99f591e6137d9452bcef670c861a8d
lc <1641767724@qq.com>
2026-06-13 22:16:00 +0800

    矩阵键盘

---
+++ 矩阵键盘-password(2026-06-13)/矩阵键盘(2026-06-13)/main.c
@@ -0,0 +1,20 @@
+#include <REGX52.H>
+#include "Delay.h"
+#include "LCD1602.h"
+#include "MatrixKey.h"
+
+unsigned char KeyNum;
+
+void main()
+{
+	LCD_Init();
+	
+	while(1)
+	{
+		KeyNum=MatrixKey();
+		if(KeyNum)
+		{
+			LCD_ShowNum(1,1,KeyNum,2);
+		}
+	}
+}
